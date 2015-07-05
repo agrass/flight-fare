f4cdab74d0fa29acaee8ed6207cc5ffe9a58138b
Antonio Grass <antonio_grass@hotmail.com>
2015-07-05 15:54:01 -0300

    add data analize

--- lan.rb
+++ lan.rb
@@ -7,30 +7,33 @@ require 'rest-client'
 require 'mail'
 
 class SearchQuery
-  @@parsers = ["LanParser"]
-  @@headles = nil
-  @@browser = nil
-  @@trip_duration = 10
-  @@scope = 100
-  @@dest = ["SCL-MVD", "SCL-LAX"]
-  @@alert_price = Hash.new
-  @@prices = Hash.new
-  def b;  @@browser;  end
-  def h;  @@headless;  end
+  def b;  @browser;  end
+  def h;  @headless;  end
 
   def initialize(browser = nil)
-      #@@headless = Headless.new
-      #h.start
-      #@@browser = Watir::Browser.start 'www.google.com'
+    @parsers = ["LanParser"]
+    @headles = nil
+    @browser = nil
+    @trip_duration = 10
+    @scope = 30
+    @dest = ["SCL-MVD", "SCL-LAX"]
+    @alert_price = Hash.new
+    @prices = Hash.new
+    open_file
+    config_email
+    #@headless = Headless.new
+    #h.start
+    #@browser = Watir::Browser.start 'www.google.com'
   end
 
   def open_file
-    @@dest = []
+    @dest = []
     File.open("dest.txt") do |file|
       file.each do |line|
-        dest,price = line.split(",")
-        @@dest << dest
-        @@alert_price[desr] = price
+        dest,price = line.gsub("\n", "").split(",")
+        price = 150 if price.blank?
+        @dest << dest
+        @alert_price[dest] = price
       end
     end
   end
@@ -38,7 +41,7 @@ class SearchQuery
   def config_email
     options = { :address              => "smtp.gmail.com",
                 :port                 => 587,
-                :domain               => 'gmail.com ',
+                :domain               => 'localhost:3000',
                 :user_name            => ENV['GMAIL_USERNAME'],
                 :password             => ENV['GMAIL_PASSWORD'],
                 :authentication       => 'plain',
@@ -51,113 +54,118 @@ class SearchQuery
 
   def send_email(msg)
     Mail.deliver do
-      to 'antoniograssb@gmail.com'
-      from 'ag4ss@gmail.com '
+      to ENV["EMAIL_FARE"]
+      from ENV["GMAIL_USERNAME"]
       subject 'price alert'
       body msg
     end
   end
 
   def start
-    @@parsers.each do |parser|
+    @parsers.each do |parser|
       c = Object.const_get(parser)
       c = c.new
-      @@dest.each do |dest|
+      @dest.each do |dest|
         dest_s = dest.split("-")
-        next if dest.size < 2
-        for i in 0..(dest.size - 2)
+        next if dest_s.size < 2
+        for i in 0..(dest_s.size - 2)
           iterate(c, dest_s[i], dest_s[i + 1])
         end
       end
     end
+    p @prices
   end
 
   def analize
+    sorted = @prices.sort_by{|k,v| v.price }
+    values = []
+    sorted[0..5].each do |val|
+      p val.to_s
+      p val[1].to_s
+      values << val[1].to_s
+    end
+    send_email(values.join("\n"))
   end
 
   def iterate(c, orig, dest)
     s_d = Date.today + 10.days
-    e_d = s_d + @@trip_duration.days
-    while Date.today + @@scope > s_d
+    e_d = s_d + @trip_duration.days
+    while Date.today + @scope > s_d
       values = c.get_values(orig, dest, s_d, e_d)
-      p values
+      send_email("ORIG #{orig}-#{dest}: #{s_d} #{e_d} #{values}") if values[0] < (@alert_price["#{orig}-#{dest}"] || 120)
       add_price(*values)
       s_d = s_d + c.step_days.days
-      e_d = s_d + @@trip_duration.days
+      e_d = s_d + @trip_duration.days
       sleep 0.2
     end
   end
 
   def add_price(price, url, orig, dest, date1, date2)
     return if price.blank?
-    if @@prices["#{orig}-#{dest}"].blank?
-      @@prices["#{orig}-#{dest}"] = PriceResult.new(price,url,orig,dest,date1,date2)
+    if @prices["#{orig}-#{dest}"].blank?
+      @prices["#{orig}-#{dest}"] = PriceResult.new(price,url,orig,dest,date1,date2)
     else
-      @@prices["#{orig}-#{dest}"].update(price,url,orig,dest,date1,date2)
+      @prices["#{orig}-#{dest}"].update(price,url,orig,dest,date1,date2)
     end
   end
 end
 
 class PriceResult
-  @@price = nil
-  @@url = nil
-  @@orig = nil
-  @@dest = nil
-  @@date1 = nil
-  @@date2 = nil
+  def to_s
+    "#{@price} ####  #{@url} #### #{@orig}-#{@dest} #{@date1} #{@date2}"
+  end
+  def price; @price; end
   def initialize(price, url, orig, dest, date1, date2)
-    @@price = price
-    @@url = url
-    @@orig = orig
-    @@dest = dest
-    @@date1 = date1
-    @@date2 = date2
+    @price = price
+    @url = url
+    @orig = orig
+    @dest = dest
+    @date1 = date1
+    @date2 = date2
   end
   def update(price,url,orig,dest,date1,date2)
     return if price.blank?
-    if @@price.blank? || @@price > price
-      @@price = price
-      @@url = url
-      @@orig = orig
-      @@dest = dest
-      @@date1 = date1
-      @@date2 = date2
+    if @price.blank? || @price > price
+      @price = price
+      @url = url
+      @orig = orig
+      @dest = dest
+      @date1 = date1
+      @date2 = date2
     end
   end
 end
 
 class PriceParser
-  @@browser = nil
-  @@headless = nil
-  @@step_days = 1
+  @browser = nil
+  @headless = nil
+  @step_days = 1
 
-  def b;  @@browser;  end
-  def h;  @@headless;  end
+  def b;  @browser;  end
+  def h;  @headless;  end
   def initialize(browser = nil)
     if browser
-      @@browser = browser
+      @browser = browser
     else
-      #@@headless = Headless.new
+      #@headless = Headless.new
       #h.start
-      #@@browser = Watir::Browser.start 'www.google.com'
+      #@browser = Watir::Browser.start 'www.google.com'
     end
   end
 end
 
 class LanParser < PriceParser
-  @@step_days = 7
-  def step_days; return @@step_days; end
+  def step_days; return @step_days; end
   def initialize(url = nil)
     super
-    p "finish"
-    #b.close
-    #h.destroy
+    @step_days = 7
   end
   def get_values(orig, dest, date1, date2)
     json = JSON.parse(RestClient.get get_url(orig, dest, date1, date2)) rescue nil
     price = 999999999999
     url = get_public_url(orig, dest, date1, date2)
-    json['data']['recomendations']['data'].each do |data|
+    query =  json['data']['recomendations']['data'] rescue []
+    query.each do |data|
       price2 = data['fare']['passengerMap']['adult']['amount'].to_i rescue price
       if price2 < price
         price = price2
@@ -182,6 +190,5 @@ class LanParser < PriceParser
   end
 end
 a = SearchQuery.new
-#a.start
-a.config_email
-a.send_email("hola")
+a.start
+a.analize
